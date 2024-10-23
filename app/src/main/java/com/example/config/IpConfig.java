f7135221b6c5cf41ed6dbd7a558e126c811f0254
Full-Stack-Sanctus <Full-Stack-Sanctus@gmail.com>
2024-10-23 07:37:31 +0000

    Pushing commit

--- app/src/main/java/com/example/config/IpConfig.java
+++ app/src/main/java/com/example/config/IpConfig.java
@@ -1,6 +1,6 @@
 package config;
 
 public class IpConfig {
-    public static final String SERVER_IP = "YOUR_USA_SERVER_IP"; // Replace with actual server IP
+    public static final String SERVER_IP = "192.168.0.4"; // Replace with actual server IP
     public static final int SERVER_PORT = 8080; // Replace with actual server port
 }
